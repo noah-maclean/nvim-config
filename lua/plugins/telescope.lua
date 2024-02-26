0be1c07277ffe926bc4d4f2d6f3ce88997ce37e8
noah-maclean <noahmac5000@hotmail.com>
2024-02-26 22:06:19 +0000

    changed a description in the telescope file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
         -- set keymaps for telescope
         -- Ctrl + p will search for files
         vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-        -- <leader>fg will search in file (grep)
+        -- <leader>fg will search in open files (grep)
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
     end
 }
